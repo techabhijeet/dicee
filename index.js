2ee1ba84408e363c6a29a0d0988dc0e97612a11e
techabhijeet <85099943+techabhijeet@users.noreply.github.com>
2021-06-10 19:45:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-let randomNumber1 = "dice" + (Math.floor(Math.random() * 6) + 1);
-let randomNumber2 = "dice" + (Math.floor(Math.random() * 6) + 1);
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomDice1 = "dice" + randomNumber1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomDice2 = "dice" + randomNumber2;
+
+// Change h1 who won and draw the match
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player1 Wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player2 Wins 🚩";
+} else {
+  document.querySelector("h1").innerHTML = " 🚩 Draw 🚩";
+}
+
+// to change the dice
 document
   .querySelector(".img1")
-  .setAttribute("src", "images/" + randomNumber1 + ".png");
+  .setAttribute("src", "images/" + randomDice1 + ".png");
 document
   .querySelector(".img2")
-  .setAttribute("src", "images/" + randomNumber2 + ".png");
+  .setAttribute("src", "images/" + randomDice2 + ".png");
